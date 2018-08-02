692a127955563c18724fd55ce2d289d72f7c2432
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-02 22:23:57 +0100

    adds breeding location radio buttons input

--- app.js
+++ app.js
@@ -7,23 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const name = event.target.name.value;
     const species = event.target.species.value;
+    const bred = event.target.bred.value;
     const continent = event.target.continent.value;
 
     const newAnimal = document.createElement("ul");
     const newAnimalName = document.createElement("li");
     const newAnimalSpecies = document.createElement("li");
+    const newAnimalBred = document.createElement("li");
     const newAnimalContinent = document.createElement("li");
 
     newAnimalName.textContent = `Name: ${name}`;
     newAnimalSpecies.textContent = `Species: ${species}`;
+    newAnimalBred.textContent = `Bred: ${bred}`;
     newAnimalContinent.textContent = `Continent: ${continent}`;
 
     newAnimal.appendChild(newAnimalName);
     newAnimal.appendChild(newAnimalSpecies);
+    newAnimal.appendChild(newAnimalBred);
     newAnimal.appendChild(newAnimalContinent);
 
     const animalList = document.querySelector('#animal-list')
     animalList.appendChild(newAnimal);
+
     form.reset();
   });
 
