e1746daf6f44832c1186de63dca9201043b5273d
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-03 14:46:48 +0100

    enables photo to be shown for new entries

--- app.js
+++ app.js
@@ -12,28 +12,32 @@ document.addEventListener('DOMContentLoaded', () => {
     event.preventDefault();
 
     const name = event.target.name.value;
+    const photo = event.target.photo.value;
     const species = event.target.species.value;
     const bred = event.target.bred.value;
     const continent = event.target.continent.value;
 
     const newAnimal = document.createElement("ul");
+
     const newAnimalName = document.createElement("li");
+    const newAnimalPhoto = document.createElement("IMG");
     const newAnimalSpecies = document.createElement("li");
     const newAnimalBred = document.createElement("li");
     const newAnimalContinent = document.createElement("li");
-    const newAnimalPhoto = document.createElement("li");
 
     newAnimalName.textContent = `Name: ${name}`;
+    newAnimalPhoto.setAttribute("src", `${photo}`);
+    newAnimalPhoto.setAttribute("width", "225rem");
+    newAnimalPhoto.setAttribute("alt", `Photo: ${species}`);
     newAnimalSpecies.textContent = `Species: ${species}`;
     newAnimalBred.textContent = `Bred: ${bred}`;
     newAnimalContinent.textContent = `Continent: ${continent}`;
-    newAnimalPhoto.textContent = `awaiting photo`;
 
     newAnimal.appendChild(newAnimalName);
+    newAnimal.appendChild(newAnimalPhoto);
     newAnimal.appendChild(newAnimalSpecies);
     newAnimal.appendChild(newAnimalBred);
     newAnimal.appendChild(newAnimalContinent);
-    newAnimal.appendChild(newAnimalPhoto);
 
     const animalList = document.querySelector('#animal-list')
     animalList.appendChild(newAnimal);
